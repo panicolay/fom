e012d6f10ac166264a97502df74c3bd2ba7d3f27
panicolay <panicolay@pm.me>
2025-03-27 07:41:07 +0100

    delete dialog ui refinement

--- src/components/overlays/DeleteDialog.tsx
+++ src/components/overlays/DeleteDialog.tsx
@@ -19,10 +19,10 @@ export function DeleteDialog({
     return (
         <Dialog open={isOpen} onClose={onClose} className="relative z-50">
             <DialogBackdrop transition className="
-                fixed inset-0 bg-base-950/40 transition ease-in-out duration-160
+                fixed inset-0 bg-base-950/80 transition ease-in-out duration-160
                 data-[closed]:opacity-0" />
 
-            <div className="fixed inset-0 flex justify-center pt-28">
+            <div className="fixed inset-0 flex justify-center pt-24">
                 <DialogPanel
                     transition
                     className="
@@ -32,26 +32,25 @@ export function DeleteDialog({
                         data-[closed]:opacity-0 data-[closed]:-translate-y-4
                         duration-160"
                 >
-                    <div className="p-6">
+                    <div className="flex flex-col p-4 gap-2">
                         <DialogTitle
                             className="
-                                mb-4
                                 text-sm text-base-400 font-display uppercase">
                             {title}
                         </DialogTitle>
                         <div className="">{text}</div>
                     </div>
-                    <div className="flex">
+                    <div className="flex h-12">
                         <Button
                             variant="panelGhost"
-                            className="w-full h-14 border-t border-r border-base-700"
+                            className="w-full border-t border-r border-base-700"
                             onClick={onClose}
                         >
                             Cancel
                         </Button>
                         <Button
                             variant="panelGhostDanger"
-                            className="w-full h-14 border-t border-base-700"
+                            className="w-full border-t border-base-700"
                             onClick={onConfirm}
                         >
                             Delete
