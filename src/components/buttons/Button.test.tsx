03a5b16539d99a72a7ab3bcb670edbc8964827c9
Klarice123 <147624431+Klarice123@users.noreply.github.com>
2025-04-09 14:43:55 +0200

    chore/set up vitest

---
+++ src/components/buttons/Button.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import { Button } from './Button'
+
+describe('Hello component', () => {
+  it('renders the name', () => {
+    render(<Button/>)
+    expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+})
+
+
