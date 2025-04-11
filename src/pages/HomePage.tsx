934957077f6f171bd2f2918b4c53f16c1b733fb9
clarice.bascou <clarice.bascou@octo.com>
2025-04-11 16:08:02 +0200

    création du service

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,26 @@
+import { useEffect } from 'react'
 import { StructureList } from '../components/structures/StructureList'
+import { signUpUser } from '../services/userService'
 
 export function HomePage() {
+  const handleSignup = async () => {
+    try {
+      const user = await signUpUser({
+        email: 'test@gmail.com',
+        password: 'password123',
+        username: 'clarice123',
+        bio: 'Dev en reconversion ✨',
+      })
+      console.log('User created :', user)
+    } catch (error) {
+      console.error('Error')
+    }
+  }
+
+  useEffect(() => {
+    handleSignup()
+  }, [])
+
   return (
     <>
       <h1 className="font-display text-7xl font-semibold text-base-200">
